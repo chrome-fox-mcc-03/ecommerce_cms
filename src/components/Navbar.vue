343d0e08118c0e7a3143281b0dd3196694f8bbe7
ramadesi1 <ramadesy.saragih@gmail.com>
2020-03-18 08:47:43 +0700

    add register and login router

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar navbar-expand bg-dark navbar-dark">
+    <div class="container d-flex justify-content-between">
+      <router-link to="/" class="navbar-brand">Ecommerce Admin</router-link>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/register" class="nav-link">Register</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/login" class="nav-link">Login</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
---
+++ src/views/Register.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Create your account</h1>
+    <form @submit.prevent="submitRegister">
+      <div class="form-group">
+        <label>Name</label>
+        <input v-model="name" type="text" class="form-control" placeholder="Enter name" />
+      </div>
+      <div class="form-group">
+        <label>Email address</label>
+        <input v-model="email" type="email" class="form-control" placeholder="Enter email" />
+      </div>
+      <div class="form-group">
+        <label>Password</label>
+        <input v-model="password" type="password" class="form-control" placeholder="Password" />
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Register',
+  data () {
+    return {
+      name: '',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitRegister () {
+      const data = {
+        name: this.name,
+        email: this.email,
+        password: this.password
+      }
+      axios.post('http://localhost:3000/register', data).then(response => {
+        console.log(response)
+      })
+    }
+  }
+}
+</script>
