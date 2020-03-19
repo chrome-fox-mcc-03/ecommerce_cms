339e7ca319255126816bdfe6b84b1de243c93384
reinfajar <tsanimfajar@outlook.com>
2020-03-19 09:01:02 +0700

    remove duplicate folder

--- ecommerce_cms/src/axios/config.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const localhost = "http://localhost:3000";
-// const heroku = "";
-const instance = axios.create({
-  baseURL: localhost
-});
-
-export default instance;
