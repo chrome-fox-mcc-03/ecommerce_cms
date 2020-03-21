240662cba8635e5c030fbb9d5404661af2117881
reinfajar <tsanimfajar@outlook.com>
2020-03-21 21:53:34 +0700

    fix not connected to server  Changes to be committed: 	modified:   .firebase/hosting.ZGlzdA.cache 	modified:   src/axios/config.js

--- client/src/axios/config.js
+++ client/src/axios/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const localhost = "http://localhost:3000";
+// const localhost = "http://localhost:3000";
 const heroku = "https://ancient-reef-30300.herokuapp.com/";
 const instance = axios.create({
   baseURL: heroku
