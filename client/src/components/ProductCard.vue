847faba09eb76173dcc6ba36a6d67106a59be82f
thomasmartuaps <thomasmartuaps@gmail.com>
2020-03-22 16:33:39 +0700

    Well done for now

---
+++ client/src/components/ProductCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="card">
+            <h5 class="card-header">{{ product.name }}</h5>
+            <div class="card-body">
+                <h5 class="card-title">{{ product.image_url }}</h5>
+                <p class="card-text">Price: {{ product.price }} Stock: {{ product.stock }}</p>
+                <a v-on:click.prevent="goToUpdate" class="btn btn-primary">Update</a> <a v-on:click.prevent="deleteItem" class="btn btn-danger">Delete</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductCard',
+  props: {
+    product: Object
+  },
+  methods: {
+    goToUpdate: function () {
+      this.$store.dispatch('getUpdatePage', this.product)
+    },
+    deleteItem: function () {
+      this.$store.dispatch('deleteItem', this.product)
+    }
+  }
+}
+</script>
+
+<style>
+.card {
+  width: 50vw;
+}
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <button class="logout" v-on:click.prevent="logout">Logout</button>
+    <br>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <addProduct/>
+    <br>
+    <ProductList/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import ProductList from '@/components/ProductList.vue'
+import AddProduct from '@/components/AddProduct.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld,
+    ProductList,
+    AddProduct
+  },
+  methods: {
+    logout: function () {
+      localStorage.clear()
+      this.$router.push({ path: 'login' })
+    }
+  }
+}
+</script>
+
+<style>
+.logout {
+  margin: 4vh 4vw;
+}
+</style>
