d181ee8f9e50dd8b1a4071766944f85e8ce4d78d
ryanadhi <ryanadhi.p@gmail.com>
2020-03-26 15:26:40 +0700

    deployed

--- src/views/CreateProduct.vue
+++ src/views/CreateProduct.vue
@@ -9,7 +9,17 @@
             <label for="price">Price:</label><br>
             <input v-model="product.price" type="number" id="price" name="price"><br>
             <label for="stock">Stock:</label><br>
-            <input v-model="product.stock" type="number" id="stock" name="stock"><br><br>
+            <input v-model="product.stock" type="number" id="stock" name="stock"><br>
+            <label for="category">Category:</label><br>
+            <select v-model="product.category">
+              <option disabled value="">Please select one</option>
+              <option value="fashion">Fashion</option>
+              <option value="food and beverages">Food and Beverages</option>
+              <option value="electronic">Electronic</option>
+              <option value="hobby">Hobby</option>
+              <option value="automotive">Automotive</option>
+              <option value="others">Others</option>
+            </select><br><br>
             <input type="submit" class="btn btn-success" value="Submit">
         </form>
     </div>
@@ -24,7 +34,8 @@ export default {
         name: '',
         image_url: '',
         price: 0,
-        stock: 0
+        stock: 0,
+        category: ''
       }
     }
   },
@@ -34,7 +45,8 @@ export default {
         name: this.product.name,
         image_url: this.product.image_url,
         price: this.product.price,
-        stock: this.product.stock
+        stock: this.product.stock,
+        category: this.product.category
       }
       this.$store.dispatch('createProduct', payload)
     }
